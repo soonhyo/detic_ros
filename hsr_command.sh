11ab9975a0a5128bb0b06621d3c2eb06312a14df
s-kim <s-kim@jsk.imi.i.u-tokyo.ac.jp>
2022-11-17 20:55:26 +0900

    add hsr_command.sh

---
+++ hsr_command.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+docker run --rm --net=host -it detic_ros:latest \
+    /bin/bash -i -c \
+    'source ~/.bashrc; \
+    rossetip; rossetmaster 133.11.216.222; \
+    roslaunch detic_ros sample_detection.launch \
+    debug:=true \
+    vocabulary:=custom \
+    custom_vocabulary:=box \
+    input_image:=/hsrb/hand_camera/color/image_raw\
+    input_depth:=/hsrb/hand_camera/aligned_depth_to_color/image_raw\
+    input_camera_info:=/hsrb/hand_camera/aligned_depth_to_color/camera_info'
